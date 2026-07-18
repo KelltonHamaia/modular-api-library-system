9d2bc887cfb68dc833a2d1271caae850238932c5
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-07-18 20:17:43 -0300

    feat: register books routes and not found route fallback

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import { bookRoutes } from '@/modules/books/http/books.routes.js'
+import { errorHandler } from '@/shared/errors/error-handler.middleware.js'
 import express from 'express'
 
 export const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use('/v1/books/', bookRoutes)
+app.use((req, res) => {
+  res.status(404).json({
+    error: `Cannot ${req.method} to ${req.originalUrl} - route not found`,
+  })
+})
+app.use(errorHandler)
