19f7bd32b87bf69ac93c8ad847022a8b3950ab09
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-07-18 20:16:04 -0300

    feat: implement service and repository from books

---
+++ src/modules/books/books.services.ts
@@ -0,0 +1,21 @@
+import { bookData } from '@/modules/books/data/books.data.js'
+import * as domain from '@/modules/books/domain/books.domain.js'
+import { BookRepository } from '@/modules/books/domain/books.repository.js'
+import { CreateBookInput } from '@/modules/books/domain/books.type.js'
+
+export const createBook = async (
+  createBookInput: CreateBookInput,
+  repository: BookRepository = bookData,
+) => {
+  const exists = await repository.titleExists(createBookInput.title)
+  domain.assertBookDoesNotExist(exists, createBookInput.title)
+
+  const newBook = await repository.createBook({
+    ...createBookInput,
+    availableCopies: domain.calculateInitialAvailableCopies(
+      createBookInput.totalCopies,
+    ),
+  })
+
+  return newBook
+}
