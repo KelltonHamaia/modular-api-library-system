3953deed4331b986b26dbadaf19e43ec8b621b1f
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-07-18 22:29:27 -0300

    refactor: change function name from validateRequest to validateRequestBody

--- src/shared/http/validate-request.shared.ts
+++ src/shared/http/validate-request.shared.ts
@@ -1,6 +1,6 @@
 import { ZodType } from 'zod/v4'
 import { Request } from 'express'
 
-export const validateRequest = <T>(schema: ZodType<T>, req: Request) => {
+export const validateRequestBody = <T>(schema: ZodType<T>, req: Request) => {
   return schema.parse(req.body)
 }
