eeb6227485a3e211c132d67808686fde8ba4ba8a
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-07-18 23:04:35 -0300

    feat: create shema validator for request sparams

--- src/shared/http/validate-request.shared.ts
+++ src/shared/http/validate-request.shared.ts
@@ -4,3 +4,7 @@ import { Request } from 'express'
 export const validateRequestBody = <T>(schema: ZodType<T>, req: Request) => {
   return schema.parse(req.body)
 }
+
+export const validateRequestParams = <T>(schema: ZodType<T>, req: Request) => {
+  return schema.parse(req.params)
+}
