1df4e03378e19b9b568a16eb4d20db214a266688
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-07-22 21:42:20 -0300

    feat add error handling for zodError instances

--- src/shared/errors/error-handler.middleware.ts
+++ src/shared/errors/error-handler.middleware.ts
@@ -1,7 +1,13 @@
 import { ErrorRequestHandler } from 'express'
 import { AppError } from '@/shared/errors/error.shared.js'
+import { ZodError, flattenError } from 'zod'
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      error: flattenError(error),
+    })
+  }
   if (error instanceof AppError) {
     return res.status(error.code).json({
       error: error.message,
